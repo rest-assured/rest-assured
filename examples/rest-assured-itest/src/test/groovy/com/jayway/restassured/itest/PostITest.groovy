3b154f19196a22f1ad220364edcca320fe33bdb6
Johan Haleby <johan.haleby@gmail.com>
2010-10-24 15:28:49 +0200

    --amend

---
+++ examples/rest-assured-itest/src/test/groovy/com/jayway/restassured/itest/PostITest.groovy
@@ -0,0 +1,18 @@
+package com.jayway.restassured.itest
+
+import org.junit.Test
+import static com.jayway.restassured.RestAssured.post
+import static groovy.util.GroovyTestCase.assertEquals
+import com.jayway.restassured.itest.support.WithJetty
+
+class PostITest extends WithJetty {
+  @Test
+  public void simplePost() throws Exception {
+    post ("/hello").then {response, json -> assertEquals "Hello Scalatra", json.hello }
+  }
+
+  @Test
+  public void simpleWithFormParameters() throws Exception {
+    post ("/greet").with().parameters([firstName: "John", lastName: "Doe"]). then {response, json -> assertEquals "Greetings John Doe", json.greeting }
+  }
+}
