ad4a2b89e91976fc2dfb346c9ee1cd550cd5bc86
Johan Haleby <johan.haleby@gmail.com>
2011-09-05 20:04:58 +0200

    Addded better javadoc

--- rest-assured/src/main/java/com/jayway/restassured/specification/RequestSpecification.java
+++ rest-assured/src/main/java/com/jayway/restassured/specification/RequestSpecification.java
@@ -745,8 +745,8 @@ public interface RequestSpecification extends RequestSender {
 
     /**
      * Specify a file to upload to the server using multi-part form data uploading.
-     * It will assume that the control name is <tt>file</tt>. If this is not
-     * what you want please use an overloaded method.
+     * It will assume that the control name is <tt>file</tt> and the mime-type is <tt>application/octet-stream</tt>.
+     * If this is not what you want please use an overloaded method.
      *
      * @param file The file to upload
      * @return The request specification
@@ -754,9 +754,9 @@ public interface RequestSpecification extends RequestSender {
     RequestSpecification multiPart(File file);
 
     /**
-     * Specify a file to upload to the server using multi-part form data uploading.
-     * It will assume that the input name attribute is <tt>file</tt>. If this is not
-     * what you want please use an overloaded method.
+     * Specify a file to upload to the server using multi-part form data uploading with a specific
+     * control name. It will use the mime-type <tt>application/octet-stream</tt>.
+     * If this is not what you want please use an overloaded method.
      *
      * @param file The file to upload
      * @param controlName Defines the control name of the body part. In HTML this is the attribute name of the input tag.
@@ -764,18 +764,79 @@ public interface RequestSpecification extends RequestSender {
      */
     RequestSpecification multiPart(String controlName, File file);
 
+    /**
+     * Specify a file to upload to the server using multi-part form data uploading with a specific
+     * control name and mime-type.
+     *
+     * @param file The file to upload
+     * @param controlName Defines the control name of the body part. In HTML this is the attribute name of the input tag.
+     * @param mimeType The mime-type
+     * @return The request specification
+     */
     RequestSpecification multiPart(String controlName, File file, String mimeType);
 
+    /**
+     * Specify a byte-array to upload to the server using multi-part form data.
+     * It will use the mime-type <tt>application/octet-stream</tt>. If this is not what you want please use an overloaded method.
+     *
+     * @param controlName Defines the control name of the body part. In HTML this is the attribute name of the input tag.
+     * @param fileName The name of the content you're uploading
+     * @param bytes The bytes you want to send
+     * @return The request specification
+     */
     RequestSpecification multiPart(String controlName, String fileName, byte[] bytes);
 
+    /**
+     * Specify a byte-array to upload to the server using multi-part form data.
+     *
+     * @param controlName Defines the control name of the body part. In HTML this is the attribute name of the input tag.
+     * @param fileName The name of the content you're uploading
+     * @param bytes The bytes you want to send
+     * @param mimeType The mime-type
+     * @return The request specification
+     */
     RequestSpecification multiPart(String controlName, String fileName, byte[] bytes, String mimeType);
 
+    /**
+     * Specify an inputstream to upload to the server using multi-part form data.
+     * It will use the mime-type <tt>application/octet-stream</tt>. If this is not what you want please use an overloaded method.
+     *
+     * @param controlName Defines the control name of the body part. In HTML this is the attribute name of the input tag.
+     * @param fileName The name of the content you're uploading
+     * @param stream The stream you want to send
+     * @return The request specification
+     */
     RequestSpecification multiPart(String controlName, String fileName, InputStream stream);
 
+    /**
+     * Specify an inputstream to upload to the server using multi-part form data.
+     *
+     * @param controlName Defines the control name of the body part. In HTML this is the attribute name of the input tag.
+     * @param fileName The name of the content you're uploading
+     * @param stream The stream you want to send
+     * @param mimeType The mime-type
+     * @return The request specification
+     */
     RequestSpecification multiPart(String controlName, String fileName, InputStream stream, String mimeType);
 
+    /**
+     * Specify a string to send to the server using multi-part form data.
+     * It will use the mime-type <tt>text/plain</tt>. If this is not what you want please use an overloaded method.
+     *
+     * @param controlName Defines the control name of the body part. In HTML this is the attribute name of the input tag.
+     * @param contentBody The string to send
+     * @return The request specification
+     */
     RequestSpecification multiPart(String controlName, String contentBody);
 
+    /**
+     * Specify a string to send to the server using multi-part form data with a specific mime-type.
+     *
+     * @param controlName Defines the control name of the body part. In HTML this is the attribute name of the input tag.
+     * @param contentBody The string to send
+     * @param mimeType The mime-type
+     * @return The request specification
+     */
     RequestSpecification multiPart(String controlName, String contentBody, String mimeType);
 
     /**
