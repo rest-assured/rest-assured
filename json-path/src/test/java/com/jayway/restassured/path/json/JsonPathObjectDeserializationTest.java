ba55f5c4fc36162b98d8a2adc31bdb23922331a4
Johan Haleby <johan.haleby@gmail.com>
2013-04-12 13:08:20 +0200

    Added some more examples

--- json-path/src/test/java/com/jayway/restassured/path/json/JsonPathObjectDeserializationTest.java
+++ json-path/src/test/java/com/jayway/restassured/path/json/JsonPathObjectDeserializationTest.java
@@ -4,13 +4,16 @@ import com.jayway.restassured.mapper.ObjectDeserializationContext;
 import com.jayway.restassured.path.json.config.JsonPathConfig;
 import com.jayway.restassured.path.json.mapping.JsonPathObjectDeserializer;
 import com.jayway.restassured.path.json.support.Greeting;
+import com.jayway.restassured.path.json.support.Winner;
 import org.apache.commons.lang3.StringUtils;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static com.jayway.restassured.path.json.JsonPath.from;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class JsonPathObjectDeserializationTest {
@@ -19,6 +22,19 @@ public class JsonPathObjectDeserializationTest {
             "                \"firstName\" : \"John\", \n" +
             "                \"lastName\" : \"Doe\" \n" +
             "               }\n" +
+            "}";
+
+    private static final String LOTTO = "{\"lotto\":{\n" +
+            "    \"lottoId\":5,\n" +
+            "    \"winning-numbers\":[2,45,34,23,7,5,3],\n" +
+            "    \"winners\":[{\n" +
+            "      \"winnerId\":23,\n" +
+            "      \"numbers\":[2,45,34,23,3,5]\n" +
+            "    },{\n" +
+            "      \"winnerId\":54,\n" +
+            "      \"numbers\":[52,3,12,11,18,22]\n" +
+            "    }]\n" +
+            "  }\n" +
             "}";
 
     @Test public void
@@ -77,4 +93,23 @@ public class JsonPathObjectDeserializationTest {
         }
     }
 
+    @Test public void
+    extracting_first_lotto_winner_to_java_object() {
+        // When
+        final Winner winner = from(LOTTO).getObject("lotto.winners[0]", Winner.class);
+
+        // Then
+        assertThat(winner.getNumbers(), hasItems(2,45,34,23,3,5));
+        assertThat(winner.getWinnerId(), is(23));
+    }
+
+    @Test public void
+    getting_numbers_greater_than_ten_for_lotto_winner_with_id_equal_to_23() {
+        // When
+        List<Integer> numbers = from(LOTTO).getList("lotto.winners.find { it.winnerId == 23 }.numbers.findAll { it > 10 }", Integer.class);
+
+        // Then
+        assertThat(numbers, hasItems(45, 34, 23));
+        assertThat(numbers, hasSize(3));
+    }
 }
