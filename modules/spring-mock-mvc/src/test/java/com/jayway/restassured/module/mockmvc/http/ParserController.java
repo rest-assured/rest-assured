0757170ae17b111ae25055c8c9f0a05601f03a12
Johan Haleby <johan.haleby@gmail.com>
2014-01-06 08:15:55 +0100

    Added test for parsers

---
+++ modules/spring-mock-mvc/src/test/java/com/jayway/restassured/module/mockmvc/http/ParserController.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jayway.restassured.module.mockmvc.http;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+public class ParserController {
+
+    @RequestMapping(value = "/parserWithUnknownContentType", method = GET, produces = "some/thing")
+    public @ResponseBody String parserWithUnknownContentType(@RequestParam(value = "param") String param) {
+        return "{ \"param\" : \"" + param + "\" }";
+    }
+}
