4f1ea34dfbd9cdcf798d81264a6947a8ed7c94ac
Johan Haleby <johan.haleby@gmail.com>
2014-03-28 11:38:57 +0100

    Added test to verify log override

--- examples/rest-assured-itest-java/src/test/java/com/jayway/restassured/itest/java/LogIfValidationFailsITest.java
+++ examples/rest-assured-itest-java/src/test/java/com/jayway/restassured/itest/java/LogIfValidationFailsITest.java
@@ -259,4 +259,28 @@ public class LogIfValidationFailsITest extends WithJetty {
                     "Content-Type: application/json; charset=UTF-8"+LINE_SEPARATOR+"Content-Length: 33"+LINE_SEPARATOR+"Server: Jetty(6.1.14)"+LINE_SEPARATOR));
           }
     }
+
+    @Test
+    public void configuredLoggingInGivenOverwritesTheLoggingSpecifiedInLogConfig() throws Exception {
+        final StringWriter writer = new StringWriter();
+        final PrintStream captor = new PrintStream(new WriterOutputStream(writer), true);
+
+        try {
+            given().
+                    config(RestAssured.config().logConfig(logConfig().defaultStream(captor).and().enableLoggingOfRequestAndResponseIfValidationFails(HEADERS))).
+                    param("firstName", "John").
+                    param("lastName", "Doe").
+                    header("Content-type", "application/json").
+                    log().all().
+            when().
+                    get("/greet").
+            then().
+                    statusCode(400);
+
+              fail("Should throw AssertionError");
+          } catch (AssertionError e) {
+            assertThat(writer.toString(), equalTo("Request method:\tGET"+LINE_SEPARATOR+"Request path:\thttp://localhost:8080/greet?firstName=John&lastName=Doe"+LINE_SEPARATOR+"Request params:\tfirstName=John"+LINE_SEPARATOR+"\t\t\t\tlastName=Doe"+LINE_SEPARATOR+"Query params:\t<none>"+LINE_SEPARATOR+"Form params:\t<none>"+LINE_SEPARATOR+"Path params:\t<none>"+LINE_SEPARATOR+"Headers:\t\tContent-Type=application/json"+LINE_SEPARATOR+"Cookies:\t\t<none>"+LINE_SEPARATOR+"Body:\t\t\t<none>" + LINE_SEPARATOR +
+                    "Content-Type: application/json; charset=UTF-8"+LINE_SEPARATOR+"Content-Length: 33"+LINE_SEPARATOR+"Server: Jetty(6.1.14)"+LINE_SEPARATOR));
+          }
+    }
 }
