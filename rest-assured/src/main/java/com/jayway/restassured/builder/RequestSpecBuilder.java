0ae036bc0b4221abb7f8ab27128e23d6e8a64f43
MerkushevKirill <lanwen@yandex-team.ru>
2014-09-20 02:44:05 +0400

    add - remove*Param methods to RequestSpecBuilder

--- rest-assured/src/main/java/com/jayway/restassured/builder/RequestSpecBuilder.java
+++ rest-assured/src/main/java/com/jayway/restassured/builder/RequestSpecBuilder.java
@@ -422,6 +422,18 @@ public class RequestSpecBuilder {
     }
 
     /**
+     * Method to remove parameter added with {@link #addParameter(String, Object...)} from map.
+     * Removes all values of this parameter
+     *
+     * @param parameterName   The parameter key
+     * @return The request specification builder
+     */
+    public RequestSpecBuilder clearParam(String parameterName) {
+        spec.clearParam(parameterName);
+        return this;
+    }
+
+    /**
      * Add query parameters to be sent with the request as a Map. This method is the same as {@link #addParameters(java.util.Map)}
      * for all HTTP methods except POST where this method can be used to differentiate between form and query params.
      *
@@ -496,6 +508,20 @@ public class RequestSpecBuilder {
         spec.queryParam(parameterName, parameterValues);
         return this;
     }
+
+
+    /**
+     * Method to remove parameter added with {@link #addQueryParameter(String, Object...)} from map.
+     * Removes all values of this parameter
+     *
+     * @param parameterName   The parameter key
+     * @return The request specification builder
+     */
+    public RequestSpecBuilder clearQueryParam(String parameterName) {
+        spec.clearQueryParam(parameterName);
+        return this;
+    }
+
 
     /**
      * Add query parameters to be sent with the request as a Map. This method is the same as {@link #addParameters(java.util.Map)}
@@ -572,6 +598,21 @@ public class RequestSpecBuilder {
         spec.formParam(parameterName, parameterValues);
         return this;
     }
+
+
+    /**
+     * Method to remove parameter added with {@link #addFormParameter(String, Object...)} from map.
+     * Removes all values of this parameter
+     *
+     * @param parameterName   The parameter key
+     * @return The request specification builder
+     */
+    public RequestSpecBuilder clearFormParam(String parameterName) {
+        spec.clearFormParam(parameterName);
+        return this;
+    }
+
+
 
     /**
      * Specify a path parameter. Path parameters are used to improve readability of the request path. E.g. instead
