8a7bfae64571315bc8988b437d64bd6007719441
Johan Haleby <johan.haleby@gmail.com>
2015-08-09 09:01:44 +0200

    Fixed issues with update_wiki script

--- update_wiki.sh
+++ update_wiki.sh
@@ -3,9 +3,25 @@ read -p "Enter the old version: " oldVersion
 read -p "Enter the new version: " newVersion
 tmpFolderRootName=/tmp/$RANDOM
 folderName=${tmpFolderRootName}/rest-assured.wiki
-git clone https://github.com/jayway/rest-assured.wiki.git ${folderName} && cd ${folderName} && 
-sed -i "" 's/${oldVersion}/${newVersion}/g' *.md && 
+
+updateFiles() {
+if [[ -z "$filesToUpdate" ]]
+    then
+        echo "Couldn't find any files to update."
+    exit 0
+fi
+
+for file in ${filesToUpdate}
+    do
+        echo "Updating $file" &&
+        sed -i "" "s/${oldVersion}/${newVersion}/g" "$file"
+    done
+}
+git clone https://github.com/jayway/rest-assured.wiki.git ${folderName} && cd ${folderName} &&
+filesToUpdate=$(find * ! -name "ReleaseNotes*.md" ! -name "OldNews.md" ! -name "How_to_release.md" -name "*.md" -type f -print) &&
+updateFiles &&
 read -p "Would you like to commit the changes? [y/N]" -n 1 -r
+echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
 	echo "Committing and pushing changes" && 
 	git commit -am "Updating docs for version ${newVersion}" && git push && echo "Changes pushed.." &&
