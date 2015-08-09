a51b1d586edcc87bec6de73a2c64aabaf57bf0a8
Johan Haleby <johan.haleby@gmail.com>
2015-08-09 09:16:43 +0200

    Added maven release script

---
+++ mvn_release.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+read -p "Enter the version to release: " releaseVersion
+echo "Starting to relase REST Assured $releaseVersion"
+
+mvn release:prepare -Prelease,dist -DautoVersionSubmodules=true -Dtag=rest-assured-${releaseVersion} -DreleaseVersion=${releaseVersion} &&
+mvn release:perform -Prelease,dist
+
+echo "Maven released of REST Assured $releaseVersion completed successfully"
+
