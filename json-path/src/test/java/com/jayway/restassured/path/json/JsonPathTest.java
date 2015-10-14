d27ae54268abad6e6694831472506e39e1d93af7
Johan Haleby <johan.haleby@gmail.com>
2015-10-14 14:17:57 +0200

    Added new example

--- json-path/src/test/java/com/jayway/restassured/path/json/JsonPathTest.java
+++ json-path/src/test/java/com/jayway/restassured/path/json/JsonPathTest.java
@@ -627,6 +627,51 @@ public class JsonPathTest {
         assertThat(jsonPath.getList("features.properties.gridId", Integer.class), hasItems(7));
     }
 
+    @Test public void
+    can_manually_escape_class_property() {
+        // Given
+        String json = "{\n" +
+                "  \"semester\": \"Fall 2015\",\n" +
+                "  \"groups\": [\n" +
+                "    {\n" +
+                "      \"siteUrl\": \"http://cphbusinessjb.cloudapp.net/CA2/\",\n" +
+                "      \"error\": \"NO AUTHOR/CLASS-INFO\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"siteUrl\": \"http://ca2-ebski.rhcloud.com/CA2New/\",\n" +
+                "      \"authors\": \"Ebbe, Kasper, Christoffer\",\n" +
+                "      \"class\": \"A klassen\",\n" +
+                "      \"group\": \"Gruppe: Johns Llama Herders A/S\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"siteUrl\": \"http://ca2-chrislind.rhcloud.com/CA2Final/\",\n" +
+                "      \"error\": \"NO AUTHOR/CLASS-INFO\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"siteUrl\": \"http://ca2-pernille.rhcloud.com/NYCA2/\",\n" +
+                "      \"authors\": \"Marta, Jeanette, Pernille\",\n" +
+                "      \"class\": \"DAT A\",\n" +
+                "      \"group\": \"Group: MJP\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"siteUrl\": \"https://ca2-afn.rhcloud.com:8443/company.jsp\",\n" +
+                "      \"error\": \"NO AUTHOR/CLASS-INFO\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"siteUrl\": \"http://ca-smcphbusiness.rhcloud.com/ca2/index.jsp\",\n" +
+                "      \"authors\": \"Mikkel, Steffen, B Andersen\",\n" +
+                "      \"class\": \"A Class Computer Science\",\n" +
+                "      \"group\": \"1\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+        // When
+        JsonPath jsonPath = new JsonPath(json);
+
+        // Then
+        assertThat(jsonPath.getList("groups.getAt('class')", String.class), hasItems("A klassen"));
+    }
+
     /**
      * Asserts that https://github.com/jayway/rest-assured/issues/556 is resolved
      */
