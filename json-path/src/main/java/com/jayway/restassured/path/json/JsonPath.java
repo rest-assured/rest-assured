7aab200970cdecaa09c3f333d2170174dd4cc451
librucha <libor.ondrusek@gmail.com>
2015-10-28 11:30:42 +0100

    Rollbacked imports

--- json-path/src/main/java/com/jayway/restassured/path/json/JsonPath.java
+++ json-path/src/main/java/com/jayway/restassured/path/json/JsonPath.java
@@ -31,21 +31,10 @@ import com.jayway.restassured.path.json.exception.JsonPathException;
 import groovy.json.JsonBuilder;
 import groovy.json.JsonOutput;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.net.URL;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.UUID;
 
 /**
  * JsonPath is an alternative to using XPath for easily getting values from a Object document. It follows the
