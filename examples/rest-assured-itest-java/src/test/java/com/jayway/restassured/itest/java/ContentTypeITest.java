bbda2ee594cc26fc03cd3bc9f0288451354133f2
Johan Haleby <johan.haleby@gmail.com>
2016-02-17 12:56:08 +0100

    Removed logging

--- examples/rest-assured-itest-java/src/test/java/com/jayway/restassured/itest/java/ContentTypeITest.java
+++ examples/rest-assured-itest-java/src/test/java/com/jayway/restassured/itest/java/ContentTypeITest.java
@@ -356,7 +356,6 @@ public class ContentTypeITest extends WithJetty {
         when().
                 get("/something").
         then().
-                log().all().
                 contentType(isEmptyOrNullString());
     }
 
