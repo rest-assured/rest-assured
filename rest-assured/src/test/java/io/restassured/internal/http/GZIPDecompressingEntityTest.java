194e9c62bf6db0fdc97718148b0dcd15b751a709
Johan Haleby <johan.haleby@gmail.com>
2017-05-02 09:25:52 +0200

    Added copyright header

--- rest-assured/src/test/java/io/restassured/internal/http/GZIPDecompressingEntityTest.java
+++ rest-assured/src/test/java/io/restassured/internal/http/GZIPDecompressingEntityTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.restassured.internal.http;
 
 import io.restassured.internal.http.GZIPEncoding.GZIPDecompressingEntity;
@@ -22,7 +37,7 @@ public class GZIPDecompressingEntityTest {
         byte[] compressed = gzipCompress(json);
 
         // When
-        GZIPDecompressingEntity yo = new GZIPDecompressingEntity(new ByteArrayEntity(compressed) {
+        GZIPDecompressingEntity gzipDecompressingEntity = new GZIPDecompressingEntity(new ByteArrayEntity(compressed) {
             @Override
             public long getContentLength() {
                 return -1;
@@ -30,7 +45,7 @@ public class GZIPDecompressingEntityTest {
         });
 
         // Then
-        String string = IOUtils.toString(yo.getContent());
+        String string = IOUtils.toString(gzipDecompressingEntity.getContent());
         assertThat(string).isEqualTo(json);
     }
 
