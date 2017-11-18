93fd9a903d202e630b655452080efefd2d06817f
Johan Haleby <johan.haleby@gmail.com>
2017-11-18 12:06:39 +0100

    REST Assured now works on Java 9

--- examples/rest-assured-itest-java/src/test/java/io/restassured/itest/java/GivenWhenThenXsdITest.java
+++ examples/rest-assured-itest-java/src/test/java/io/restassured/itest/java/GivenWhenThenXsdITest.java
@@ -23,7 +23,6 @@ import io.restassured.itest.java.support.WithJetty;
 import io.restassured.response.Response;
 import io.restassured.specification.FilterableRequestSpecification;
 import io.restassured.specification.FilterableResponseSpecification;
-import com.sun.org.apache.xerces.internal.dom.DOMInputImpl;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.w3c.dom.ls.LSInput;
@@ -31,6 +30,7 @@ import org.w3c.dom.ls.LSResourceResolver;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Reader;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
@@ -94,7 +94,88 @@ public class GivenWhenThenXsdITest extends WithJetty {
     private static class ClasspathResourceResolver implements LSResourceResolver {
 
         public LSInput resolveResource(String type, String namespaceURI, String publicId, String systemId, String baseURI) {
-            return new DOMInputImpl(publicId, systemId, baseURI, load(systemId), null);
+            
+            return new LSInput() {
+                @Override
+                public Reader getCharacterStream() {
+                    return null;
+                }
+
+                @Override
+                public void setCharacterStream(Reader characterStream) {
+
+                }
+
+                @Override
+                public InputStream getByteStream() {
+                    return load(systemId);
+                }
+
+                @Override
+                public void setByteStream(InputStream byteStream) {
+
+                }
+
+                @Override
+                public String getStringData() {
+                    return null;
+                }
+
+                @Override
+                public void setStringData(String stringData) {
+
+                }
+
+                @Override
+                public String getSystemId() {
+                    return systemId;
+                }
+
+                @Override
+                public void setSystemId(String systemId) {
+
+                }
+
+                @Override
+                public String getPublicId() {
+                    return publicId;
+                }
+
+                @Override
+                public void setPublicId(String publicId) {
+
+                }
+
+                @Override
+                public String getBaseURI() {
+                    return baseURI;
+                }
+
+                @Override
+                public void setBaseURI(String baseURI) {
+
+                }
+
+                @Override
+                public String getEncoding() {
+                    return null;
+                }
+
+                @Override
+                public void setEncoding(String encoding) {
+
+                }
+
+                @Override
+                public boolean getCertifiedText() {
+                    return false;
+                }
+
+                @Override
+                public void setCertifiedText(boolean certifiedText) {
+
+                }
+            };
         }
     }
 }
