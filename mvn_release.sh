9003daf91e743378b29ef2c19ba680f24806e117
Johan Haleby <johan.haleby@gmail.com>
2018-10-26 18:32:39 +0200

    Including osgi-tests profile in release script

--- mvn_release.sh
+++ mvn_release.sh
@@ -2,8 +2,8 @@
 read -p "Enter the version to release: " releaseVersion
 echo "Starting to relase REST Assured $releaseVersion"
 
-mvn release:prepare -Prelease,dist -DautoVersionSubmodules=true -Dtag=rest-assured-${releaseVersion} -DreleaseVersion=${releaseVersion} &&
-mvn release:perform -Prelease,dist
+mvn release:prepare -Prelease,dist,osgi-tests -DautoVersionSubmodules=true -Dtag=rest-assured-${releaseVersion} -DreleaseVersion=${releaseVersion} &&
+mvn release:perform -Prelease,dist,osgi-tests
 
 echo "Maven release of REST Assured $releaseVersion completed successfully"
 
