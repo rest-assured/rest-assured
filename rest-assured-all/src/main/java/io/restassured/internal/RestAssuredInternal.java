c105c86b56c057a905a0c18e5b6d6f11f756eb7c
Johan Haleby <johan.haleby@gmail.com>
2019-01-11 10:02:20 +0100

    Adding dummy classes to allow nexus to accept the rest-assured-all project

---
+++ rest-assured-all/src/main/java/io/restassured/internal/RestAssuredInternal.java
@@ -0,0 +1,9 @@
+package io.restassured.internal;
+
+/**
+ * A dummy class used to produce javadoc required by Sonatype Nexus to be able to publish to Maven central.
+ */
+public final class RestAssuredInternal {
+    private RestAssuredInternal() {
+    }
+}
---
+++ rest-assured-all/src/main/java/io/restassured/internal/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The main package of the rest-assured-all project
+ *
+ * This javadoc is mainly here to allow publishing the project to Maven central.
+ */
+package io.restassured.internal;
