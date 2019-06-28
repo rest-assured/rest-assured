f7bc4486c79de31b54d1aa72e9a8521699155b74
Johan Haleby <johan.haleby@gmail.com>
2019-06-28 14:21:33 +0200

    No longer depending on hamcrest-core or hamcrest-library, instead the hamcrest module is used directly.

--- rest-assured-common/src/test/java/io/restassured/internal/path/ObjectConverterTest.java
+++ rest-assured-common/src/test/java/io/restassured/internal/path/ObjectConverterTest.java
@@ -21,7 +21,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class ObjectConverterTest {
