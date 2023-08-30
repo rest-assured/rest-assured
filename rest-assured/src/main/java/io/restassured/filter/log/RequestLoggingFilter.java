e9829ef9939ffe8c310cdefea9527395439e204e
Aleksei Bekh-Ivanov <4264331+bekh6ex@users.noreply.github.com>
2023-08-30 09:59:20 +0200

    Make header filtering case-insensitive
    Many load balancers override header values, and developers grow a habit to ignore case sensitivity of the headers.
    
    This can prevent many security incidents of type "credentials in build pipeline logs".

--- rest-assured/src/main/java/io/restassured/filter/log/RequestLoggingFilter.java
+++ rest-assured/src/main/java/io/restassured/filter/log/RequestLoggingFilter.java
@@ -27,9 +27,9 @@ import org.apache.commons.lang3.Validate;
 import java.io.PrintStream;
 import java.nio.charset.Charset;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import static io.restassured.filter.log.LogDetail.ALL;
 import static io.restassured.filter.log.LogDetail.STATUS;
@@ -127,7 +127,9 @@ public class RequestLoggingFilter implements Filter {
         }
         this.stream = stream;
         this.logDetail = logDetail;
-        this.blacklistedHeaders = new HashSet<>(blacklistedHeaders);
+        TreeSet<String> caseInsensitiveBlacklistedHeaders = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+        caseInsensitiveBlacklistedHeaders.addAll(blacklistedHeaders);
+        this.blacklistedHeaders = caseInsensitiveBlacklistedHeaders;
         this.shouldPrettyPrint = shouldPrettyPrint;
         this.showUrlEncodedUri = showUrlEncodedUri;
     }
