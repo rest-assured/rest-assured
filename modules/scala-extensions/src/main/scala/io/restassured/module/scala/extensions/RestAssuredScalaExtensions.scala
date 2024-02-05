3ba073b64bfb5e8d9859561deb4e83e5f123a509
Carlos de Paula <me@carlosedp.com>
2024-02-05 16:11:16 -0300

    Create scala-extensions module for Scala 3

---
+++ modules/scala-extensions/src/main/scala/io/restassured/module/scala/extensions/RestAssuredScalaExtensions.scala
@@ -0,0 +1,179 @@
+package io.restassured.module.scala.extensions
+
+import scala.util.chaining.*
+import scala.reflect.ClassTag
+import io.restassured.RestAssured.`given`
+import io.restassured.RestAssured.`when`
+import io.restassured.internal.{ResponseSpecificationImpl, ValidatableResponseImpl}
+import io.restassured.response.{ExtractableResponse, Response, ValidatableResponse}
+import io.restassured.specification.{RequestSender, RequestSpecification}
+
+// Main wrappers
+
+/**
+ * A wrapper around [given] that starts building the request part of the test.
+ * E.g.
+ * {{{
+ * Given(_.params("firstName", "John"))
+ *  .When(_.post("/greetXML"))
+ *  .Then(res =>
+ *    res.statusCode(200)
+ *    res.body("greeting.firstName", equalTo("John"))
+ * )
+ * }}}
+ * will send a POST request to "/greetXML" with request parameters
+ * `firstName=John` and `lastName=Doe` and expect that the response body
+ * containing JSON or XML firstName equal to John.
+ *
+ * @return
+ *   A request specification.
+ *
+ * @see
+ *   io.restassured.RestAssured.given
+ */
+def Given(block: RequestSpecification => RequestSpecification): RequestSpecification =
+    `given`().tap(block)
+
+/**
+ * A wrapper around [given] that starts building the request part of the test.
+ * This is an overloaded version of [Given] that does not take any arguments.
+ * E.g.
+ * {{{
+ * Given()
+ *  .When(_.post("/greetXML"))
+ *  .Then(_.statusCode(200))
+ * }}}
+ * will send a POST request to "/greetXML" request parameters and expect that
+ * the statusCode 200.
+ *
+ * @return
+ *   A request specification.
+ *
+ * @see
+ *   io.restassured.RestAssured.given
+ */
+def Given(): RequestSpecification =
+    `given`()
+
+/**
+ * A wrapper around [io.restassured.RestAssured.when] to start building the DSL
+ * expression by sending a request without any parameters or headers etc. E.g.
+ * {{{
+ * Given()
+ *   .When(_.get("/x"))
+ *   .Then(_.body("x.y.z1", equalTo("Z1")))
+ * }}}
+ * Note that if you need to add parameters, headers, cookies or other request
+ * properties use the [[Given()]] method.
+ *
+ * @see
+ *   io.restassured.RestAssured.when
+ * @return
+ *   A request sender interface that let's you call resources on the server
+ */
+extension (spec: RequestSpecification)
+    infix def When(block: RequestSpecification => Response): Response =
+        block(spec.`when`())
+
+/**
+ * A wrapper around [io.restassured.RestAssured.when] to start building the DSL
+ * expression by sending a request without any parameters or headers etc. E.g.
+ * {{{
+ * Given()
+ *   .When(_.get("/x"))
+ *   .Then(_.body("x.y.z1", equalTo("Z1")))
+ * }}}
+ * Note that if you need to add parameters, headers, cookies or other request
+ * properties use the [[Given()]] method.
+ *
+ * @see
+ *   io.restassured.RestAssured.when
+ * @return
+ *   A request sender interface that let's you call resources on the server
+ */
+def When(block: RequestSender => Response): Response =
+    block(`when`())
+
+
+/**
+ * A wrapper around [then] that lets you validate the response.
+ * Usage example:
+ * {{{
+ * Given(_.params("firstName", "John")
+ *  .When(_.post("/greetXML"))
+ *  .Then(_.body("greeting.firstName", equalTo("John")))
+ * }}}
+ *
+ * @return
+ *   A validatable response
+ */
+extension (resp: Response)
+    infix def Then(block: ValidatableResponse => Unit): ValidatableResponse =
+        resp.`then`()
+        .tap(block)
+
+/**
+ * A wrapper around [ExtractableResponse] that lets you validate the response.
+ * Usage example:
+ * {{{
+ * val firstName: String = Given(_.params("firstName", "John")
+ *  .When(_.post("/greetXML"))
+ *  .Then(_.body("greeting.firstName", equalTo("John")))
+ *  .Extract(_.path("greeting.firstName"))
+ * }}}
+ * The above code will send a POST request to "/greetXML" with request parameters
+ * `firstName=John` and `lastName=Doe` and expect that the response body
+ * containing JSON or XML firstName equal to John.
+ * The response is then validated and the firstName is extracted from the response.
+ * The extracted firstName is then returned. The type of the extracted value is
+ * needs to be specified as a type parameter.
+ *
+ * @return
+ *   The extracted value
+ */
+extension [T]( resp: Response )
+    infix def Extract(
+        block: ExtractableResponse[Response] => T
+      )(
+        using ClassTag[T]
+      ): T =
+        val res = resp.`then`().extract().pipe(block)
+        summon[ClassTag[T]].runtimeClass match
+            case cls if cls.isAssignableFrom(res.getClass) => res.asInstanceOf[T]
+            case _                                         => throw new Exception("Unsupported type")
+
+/**
+ * A wrapper around [ValidatableResponse] that lets you validate the response.
+ * Usage example:
+ * {{{
+ * val firstName: String = Given(_.params("firstName", "John")
+ *  .When(_.post("/greetXML"))
+ *  .Then(_.body("greeting.firstName", equalTo("John")))
+ *  .Extract(_.path("greeting.firstName"))
+ * }}}
+ * The above code will send a POST request to "/greetXML" with request parameters
+ * `firstName=John` and `lastName=Doe` and expect that the response body
+ * containing JSON or XML firstName equal to John.
+ * The response is then validated and the firstName is extracted from the response.
+ * The extracted firstName is then returned. The type of the extracted value is
+ * needs to be specified as a type parameter.
+ *
+ * @return
+ *   The extracted value
+ */
+extension [T]( resp: ValidatableResponse )
+    infix def Extract(
+        block: ExtractableResponse[Response] => T
+      )(
+        using ClassTag[T]
+      ): T =
+        val res = resp.extract().pipe(block)
+        summon[ClassTag[T]].runtimeClass match
+            case cls if cls.isAssignableFrom(res.getClass) => res.asInstanceOf[T]
+            case _                                         => throw new Exception("Unsupported type")
+
+// End main wrappers
+
+private def doIfValidatableResponseImpl(fn: ResponseSpecificationImpl => Unit): ValidatableResponse => Unit =
+    resp =>
+        if resp.isInstanceOf[ValidatableResponseImpl] then fn(resp.asInstanceOf[ValidatableResponseImpl].responseSpec)
---
+++ modules/scala-extensions/src/test/scala/io/restassured/module/scala/extensions/RestAssuredScalaExtensionsTest.scala
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.restassured.module.scala.extensions
+
+import io.restassured.RestAssured
+import io.restassured.builder.ResponseBuilder
+import io.restassured.filter.Filter
+import io.restassured.http.ContentType.JSON
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchThrowable
+import org.hamcrest.Matchers.*
+import org.junit.Before
+import org.junit.Test
+
+class RestAssuredScalaExtensionsTest:
+
+    @Before
+    def `rest assured is configured`: Unit = {
+      RestAssured.filters((_, _, _) => {
+        new ResponseBuilder() .setStatusCode(200) .setContentType(JSON) .setBody("""{ "message" : "Hello World"}""").build()
+      })
+    }
+
+
+    @Test
+    def `rest assured is reset after each test`: Unit =
+        RestAssured.reset()
+
+    @Test
+    def `basic rest assured scala extensions are compilable`: Unit =
+        Given(req =>
+            req.port(7000)
+            req.header("Header", "Header")
+            req.body("hello")
+        )
+        .When(
+            _.put("/the/path")
+        )
+        .Then(res =>
+            res.statusCode(200)
+            res.body("message", equalTo("Hello World"))
+        )
+
+    @Test
+    def `extraction with rest assured scala extensions`: Unit =
+        val message: String = Given(req =>
+            req.port(7000)
+            req.header("Header", "Header")
+            req.body("hello")
+        )
+        .When(
+            _.put("/the/path")
+        )
+        .Extract(
+            _.path("message")
+        )
+        assertThat(message).isEqualTo("Hello World")
+
+    @Test
+    def `extraction after 'then', when path is not used in 'Then',  with rest assured scala extensions`: Unit =
+        val message: String = Given(req =>
+            req.port(7000)
+            req.header("Header", "Header")
+            req.body("hello")
+            req.filter((_, _, _) =>
+                new ResponseBuilder().setStatusCode(200).setContentType(JSON).setBody("""{ "message" : "Hello World"}""").build()
+            )
+        )
+        .When(
+            _.put("/the/path")
+        )
+        .Then(
+            _.statusCode(200)
+        )
+        .Extract(
+            _.path("message")
+        )
+        assertThat(message).isEqualTo("Hello World")
+
+    @Test
+    def `extraction after 'then', when path is used in 'Then',  with rest assured scala extensions`: Unit =
+        val message: String = Given(req =>
+            req.port(7000)
+            req.header("Header", "Header")
+            req.body("hello")
+            req.filter((_, _, _) =>
+                new ResponseBuilder().setStatusCode(200).setContentType(JSON).setBody("""{ "message" : "Hello World"}""").build()
+            )
+        )
+        .When(
+            _.put("/the/path")
+        )
+        .Then(res =>
+            res.statusCode(200)
+            res.body("message", not(emptyOrNullString()))
+        )
+        .Extract(
+            _.path("message")
+        )
+        assertThat(message).isEqualTo("Hello World")
+
+    @Test
+    def `all expectations error messages are included in the error message`: Unit =
+        try {
+            Given(req =>
+                req.port(7000)
+                req.header("Header", "Header")
+                req.body("hello")
+            )
+            .When(
+                _.put("/the/path")
+            )
+            .Then(res =>
+                res.statusCode(400)
+                res.body("message", equalTo("Another World"))
+                res.body("message", equalTo("Brave new world"))
+            )
+        } catch {
+            case e: AssertionError =>
+                assertThat(e).hasMessage(
+                    """1 expectation failed.
+                      |Expected status code <400> but was <200>.
+                      |""".stripMargin)
+        }
