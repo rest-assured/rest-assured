9f99cc3a211c1de0efc0315bea7acad44ca28de7
Johan Haleby <johan.haleby@gmail.com>
2024-07-05 15:17:12 +0200

    [ci skip] Removing dist from release targets since we don't upload the "dists" anymore

--- mvn_release.sh
+++ mvn_release.sh
@@ -2,8 +2,8 @@
 read -p "Enter the version to release: " releaseVersion
 echo "Starting to relase REST Assured $releaseVersion"
 
-mvn release:prepare -Prelease,dist,osgi-tests -DautoVersionSubmodules=true -Dtag=rest-assured-${releaseVersion} -DreleaseVersion=${releaseVersion} &&
-mvn release:perform -Prelease,dist,osgi-tests
+mvn release:prepare -Prelease,osgi-tests -DautoVersionSubmodules=true -Dtag=rest-assured-${releaseVersion} -DreleaseVersion=${releaseVersion} &&
+mvn release:perform -Prelease,osgi-tests
 
 echo "Maven release of REST Assured $releaseVersion completed successfully"
 
