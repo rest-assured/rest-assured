54d15a6851ac4a3403ebfbfacb293a934a131f7f
Johan Haleby <johan.haleby@gmail.com>
2025-05-22 11:05:06 +0200

    Allow skipping tests when making release

--- mvn_release.sh
+++ mvn_release.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-read -p "Enter the version to release: " releaseVersion
-echo "Starting to relase REST Assured $releaseVersion"
 
-mvn release:prepare -Prelease,osgi-tests -DautoVersionSubmodules=true -Dtag=rest-assured-${releaseVersion} -DreleaseVersion=${releaseVersion} &&
-mvn release:perform -Prelease,osgi-tests
+# Default to false if SKIP_TESTS is not set
+skipTests=${SKIP_TESTS:-false}
+echo "To skip tests, run the script like this: SKIP_TESTS=true $(basename "$0")"
+read -p "Enter the version to release: " releaseVersion
+echo "Starting to release REST Assured $releaseVersion (skipTests=${skipTests})"
+
+mavenArguments="-DskipTests=${skipTests}"
+mvn release:prepare -Prelease,osgi-tests -DautoVersionSubmodules=true -Dtag=rest-assured-${releaseVersion} -DreleaseVersion=${releaseVersion} -Darguments="${mavenArguments}"&&
+mvn release:perform -Prelease,osgi-tests -Darguments="${mavenArguments}"
 
 echo "Maven release of REST Assured $releaseVersion completed successfully"
 
