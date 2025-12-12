6d9362c1f58d9ac0e6b811f8efcc8e3eaec0472e
Johan Haleby <johan.haleby@gmail.com>
2025-12-12 11:25:41 +0100

    Ability to exclude osgi-tests in release

--- mvn_release.sh
+++ mvn_release.sh
@@ -2,16 +2,26 @@
 
 # Default to false if SKIP_TESTS is not set
 skipTests=${SKIP_TESTS:-false}
+skipOsgi=${SKIP_OSGI:-false}
 
 if [ "$skipTests" != "true" ]; then
   echo "To skip tests, run the script like this: SKIP_TESTS=true $(basename "$0")"
+fi
+
+if [ "$skipOsgi" != "true" ]; then
+  echo "To skip OSGi, run the script like this: SKIP_OSGI=true $(basename "$0")"
 fi
 
 read -p "Enter the version to release: " releaseVersion
 echo "Starting to release REST Assured $releaseVersion (skipTests=${skipTests})"
 
 mavenArguments="-DskipTests=${skipTests}"
-mvn release:prepare release:perform -Prelease,osgi-tests -DautoVersionSubmodules=true -Dtag=rest-assured-${releaseVersion} -DreleaseVersion=${releaseVersion} -Darguments="${mavenArguments}" &&
+releaseProfiles="-Prelease"
+if [ "$skipOsgi" != "true" ]; then
+  releaseProfiles="${releaseProfiles},osgi-tests"
+fi
+
+mvn release:prepare release:perform ${releaseProfiles} -DautoVersionSubmodules=true -Dtag=rest-assured-${releaseVersion} -DreleaseVersion=${releaseVersion} -Darguments="${mavenArguments}" &&
 git checkout rest-assured-${releaseVersion} &&
 mvn deploy -Prelease -DskipTests=true
 
